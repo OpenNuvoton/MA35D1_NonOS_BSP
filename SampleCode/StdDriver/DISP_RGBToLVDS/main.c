495827d1e496fe46072780f447b2553e35c1a482
MS10 CFLi0 <CFLi0@nuvoton.com>
2023-07-18 17:58:16 +0800

    Add DISP_RGBToLVDS sample code
    Change-Id: I3fc621b302a4763f5aaae00cceb0706a626df351

---
+++ SampleCode/StdDriver/DISP_RGBToLVDS/main.c
@@ -0,0 +1,149 @@
+/**************************************************************************//**
+ * @file     main.c
+ *
+ * @brief    Demonstrate MA35D1 Display Controller RGB interface connect to LVDS
+ *           for displaying.
+ *
+ * @copyright (C) 2023 Nuvoton Technology Corp. All rights reserved.
+ ******************************************************************************/
+#include <stdio.h>
+#include "NuMicro.h"
+#include "displib.h"
+
+#define DDR_ADR_FRAMEBUFFER   0x88000000UL
+
+extern uint32_t ImageDataBase, ImageDataLimit;
+
+/* LCD attributes */
+DISP_LCD_INFO LcdPanelInfo =
+{
+    /* Panel Resolution */
+    1920,
+    1200,
+    /* DISP_LCD_TIMING */
+    {
+        148000000,
+        1920,
+        32,
+        16,
+        16,
+        1200,
+        15,
+        18,
+        2,
+        ePolarity_Positive,
+        ePolarity_Positive
+    },
+    /* DISP_PANEL_CONF */
+    {
+        eDPIFmt_D24,
+        ePolarity_Positive,
+        ePolarity_Positive,
+        ePolarity_Positive
+    },
+};
+
+void SYS_Init(void)
+{
+    uint32_t pixelclk;
+
+    /* Unlock protected registers */
+    SYS_UnlockReg();
+
+    /* Enable IP clock */
+    CLK_EnableModuleClock(UART0_MODULE);
+
+    /* Select IP clock source */
+    CLK_SetModuleClock(UART0_MODULE, CLK_CLKSEL2_UART0SEL_HXT, CLK_CLKDIV1_UART0(1));
+
+    /* Update System Core Clock */
+    /* User can use SystemCoreClockUpdate() to calculate SystemCoreClock. */
+    SystemCoreClockUpdate();
+
+    /* Set GPE multi-function pins for UART0 RXD and TXD */
+    SYS->GPE_MFPH &= ~(SYS_GPE_MFPH_PE14MFP_Msk | SYS_GPE_MFPH_PE15MFP_Msk);
+    SYS->GPE_MFPH |= (SYS_GPE_MFPH_PE14MFP_UART0_TXD | SYS_GPE_MFPH_PE15MFP_UART0_RXD);
+
+}
+
+void DISP_Open(void)
+{
+    /* Set EPLL/2 as DISP Core Clock source */
+    DISP_EnableDCUClk();
+
+    /* Waiting EPLL ready */
+    CLK_WaitClockReady(CLK_STATUS_STABLE_EPLL);
+
+    /* Enable DISP Core Clock */
+    CLK_EnableModuleClock(DCU_MODULE);
+
+    /* Select DISP pixel clock source to VPLL */
+    DISP_GeneratePixelClk(LcdPanelInfo.sLcdTiming.u32PCF);
+
+    /* Select DISP pixel clock source */
+    CLK_SetModuleClock(DCUP_MODULE, CLK_CLKSEL0_DCUSEL_EPLL_DIV2, 0);
+
+    /* Set multi-function pins for LCD Display Controller */
+    SYS->GPG_MFPH = SYS->GPG_MFPH & ~(SYS_GPG_MFPH_PG8MFP_Msk | SYS_GPG_MFPH_PG9MFP_Msk | SYS_GPG_MFPH_PG10MFP_Msk) |
+                    (SYS_GPG_MFPH_PG8MFP_LCM_VSYNC | SYS_GPG_MFPH_PG9MFP_LCM_HSYNC | SYS_GPG_MFPH_PG10MFP_LCM_CLK);
+    SYS->GPK_MFPL = SYS->GPK_MFPL & ~(SYS_GPK_MFPL_PK4MFP_Msk) | SYS_GPK_MFPL_PK4MFP_LCM_DEN;
+
+    SYS->GPI_MFPH = SYS->GPI_MFPH & ~(SYS_GPI_MFPH_PI8MFP_Msk | SYS_GPI_MFPH_PI9MFP_Msk | SYS_GPI_MFPH_PI10MFP_Msk | SYS_GPI_MFPH_PI11MFP_Msk |
+                     SYS_GPI_MFPH_PI12MFP_Msk | SYS_GPI_MFPH_PI13MFP_Msk | SYS_GPI_MFPH_PI14MFP_Msk | SYS_GPI_MFPH_PI15MFP_Msk);
+    SYS->GPI_MFPH |= (SYS_GPI_MFPH_PI8MFP_LCM_DATA0 | SYS_GPI_MFPH_PI9MFP_LCM_DATA1 | SYS_GPI_MFPH_PI10MFP_LCM_DATA2 |
+                      SYS_GPI_MFPH_PI11MFP_LCM_DATA3 | SYS_GPI_MFPH_PI12MFP_LCM_DATA4 | SYS_GPI_MFPH_PI13MFP_LCM_DATA5 | SYS_GPI_MFPH_PI14MFP_LCM_DATA6 | SYS_GPI_MFPH_PI15MFP_LCM_DATA7);
+    SYS->GPH_MFPL = SYS->GPH_MFPL & ~(SYS_GPH_MFPL_PH0MFP_Msk | SYS_GPH_MFPL_PH1MFP_Msk | SYS_GPH_MFPL_PH2MFP_Msk | SYS_GPH_MFPL_PH3MFP_Msk |
+                    SYS_GPH_MFPL_PH4MFP_Msk | SYS_GPH_MFPL_PH5MFP_Msk | SYS_GPH_MFPL_PH6MFP_Msk | SYS_GPH_MFPL_PH7MFP_Msk);
+    SYS->GPH_MFPL |= (SYS_GPH_MFPL_PH0MFP_LCM_DATA8 | SYS_GPH_MFPL_PH1MFP_LCM_DATA9 | SYS_GPH_MFPL_PH2MFP_LCM_DATA10 | SYS_GPH_MFPL_PH3MFP_LCM_DATA11 |
+                      SYS_GPH_MFPL_PH4MFP_LCM_DATA12 | SYS_GPH_MFPL_PH5MFP_LCM_DATA13 | SYS_GPH_MFPL_PH6MFP_LCM_DATA14 | SYS_GPH_MFPL_PH7MFP_LCM_DATA15);
+
+    SYS->GPC_MFPH = SYS->GPC_MFPH & ~(SYS_GPC_MFPH_PC12MFP_Msk | SYS_GPC_MFPH_PC13MFP_Msk | SYS_GPC_MFPH_PC14MFP_Msk | SYS_GPC_MFPH_PC15MFP_Msk);
+    SYS->GPC_MFPH |= (SYS_GPC_MFPH_PC12MFP_LCM_DATA16 | SYS_GPC_MFPH_PC13MFP_LCM_DATA17 | SYS_GPC_MFPH_PC14MFP_LCM_DATA18 | SYS_GPC_MFPH_PC15MFP_LCM_DATA19);
+    SYS->GPH_MFPH = SYS->GPH_MFPH & ~(SYS_GPH_MFPH_PH12MFP_Msk | SYS_GPH_MFPH_PH13MFP_Msk | SYS_GPH_MFPH_PH14MFP_Msk | SYS_GPH_MFPH_PH15MFP_Msk);
+    SYS->GPH_MFPH |= (SYS_GPH_MFPH_PH12MFP_LCM_DATA20 | SYS_GPH_MFPH_PH13MFP_LCM_DATA21 | SYS_GPH_MFPH_PH14MFP_LCM_DATA22 | SYS_GPH_MFPH_PH15MFP_LCM_DATA23);
+
+    /* Configure PK.5 as Output mode */
+    GPIO_SetMode(PK, BIT5, GPIO_MODE_OUTPUT);
+    /* Set PK.5 output pin value is high */
+    PK5 = 1;
+}
+
+int main(void)
+{
+    uint32_t file_size;
+
+    /* Init System, IP clock and multi-function I/O
+       In the end of SYS_Init() will issue SYS_LockReg()
+       to lock protected register. If user want to write
+       protected register, please issue SYS_UnlockReg()
+       to unlock protected register if necessary */
+    SYS_Init();
+
+    /* Init UART to 115200-8n1 for print message */
+    UART_Open(UART0, 115200);
+
+    /* Open DISP IP Clock and set multi-function pins */
+    DISP_Open();
+
+    /* Assign the highest AXI port priority to Display */
+    DISPLIB_DDR_AXIPort_Priority();
+
+    sysprintf("\n+------------------------------------------------------------------------+\n");
+    sysprintf("|      This sample code shows RGB888 image by RGB to LVDS transmitter    |\n");
+    sysprintf("+------------------------------------------------------------------------+\n");
+
+    file_size = ptr_to_u32(&ImageDataLimit) - ptr_to_u32(&ImageDataBase);
+
+    /* Prepare DISP Framebuffer image */
+    memcpy((void *)(nc_ptr(DDR_ADR_FRAMEBUFFER)), (void *)(nc_ptr(&ImageDataBase)), file_size);
+
+    /* Configure display attributes of LCD panel */
+    DISPLIB_LCDInit(LcdPanelInfo);
+
+    DISPLIB_SetFBConfig(eFBFmt_A8R8G8B8, LcdPanelInfo.u32ResolutionWidth, LcdPanelInfo.u32ResolutionHeight, DDR_ADR_FRAMEBUFFER);
+
+    /* Start to display */
+    DISPLIB_EnableOutput(eLayer_Video);
+
+    while(1);
+}
