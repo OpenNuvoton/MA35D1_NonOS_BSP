07d07ea6a6062c52c9ead8e2aabc93eb12283e30
mjchen <mjchen@nuvoton.com>
2023-08-22 14:11:54 +0800

    Fix read register error issues
    Change-Id: Id3c595be94a02bec849b11945c855c7e60b499d4

--- Library/StdDriver/inc/canfd.h
+++ Library/StdDriver/inc/canfd.h
@@ -377,8 +377,7 @@ typedef struct
 } CANFD_TX_EVNT_ELEM_T;
 
 /// @cond HIDDEN_SYMBOLS
-#define CANFD_READ_REG_TIMEOUT    32                 /*!< CANFD read register time-out count */
-#define CANFD_REG_READ_TIME       2
+#define CANFD_REG_READ_TIME       3
 
 __STATIC_INLINE uint32_t CANFD_ReadReg(uint32_t u32RegAddr);
 
@@ -386,47 +385,24 @@ __STATIC_INLINE uint32_t CANFD_ReadReg(uint32_t u32RegAddr)
 {
     uint32_t u32ReadReg;
     uint32_t u32ReadReg_1;
-    uint32_t u32TimeOutCnt = CANFD_READ_REG_TIMEOUT;
-    uint32_t u32TimeOutCnt_1 = 0;
-    u32ReadReg = 0UL;
+    uint32_t u32TimeOutCnt = 0;
 
-    do{
-        u32ReadReg = inpw(ptr_to_u32(u32RegAddr));
+    u32ReadReg = inpw(ptr_to_u32(u32RegAddr));
 
-        if(u32ReadReg != 0x0)
+    while(u32TimeOutCnt < CANFD_REG_READ_TIME)
+    {
+        u32ReadReg_1 = inpw(ptr_to_u32(u32RegAddr));
+
+        if(u32ReadReg_1 == u32ReadReg)
         {
-            while(1)
-            {
-                u32ReadReg_1 = inpw(ptr_to_u32(u32RegAddr));
-
-                if(u32ReadReg_1 == 0x0)
-                {
-                    u32TimeOutCnt_1 = 0;
-                    break;
-                }
-
-                if(u32ReadReg_1 == u32ReadReg)
-                {
-                    u32TimeOutCnt_1++;
-                }
-                else
-                {
-                    u32ReadReg = u32ReadReg_1;
-                    u32TimeOutCnt_1 = 0;
-                }
-
-                if(u32TimeOutCnt_1 > CANFD_REG_READ_TIME)
-                {
-                    return u32ReadReg;
-                }
-            }
+            u32TimeOutCnt++;
         }
-
-        if(--u32TimeOutCnt == 0UL)
+        else
         {
-            break;
+            u32ReadReg = u32ReadReg_1;
+            u32TimeOutCnt = 0;
         }
-    }while(u32ReadReg == 0UL);
+    }
 
     return u32ReadReg;
 }
