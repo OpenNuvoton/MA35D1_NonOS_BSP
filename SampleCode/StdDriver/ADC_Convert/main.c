32be2a2d76c09d52adfe57a21a0781310e6d3013
yclu4 <yclu4@nuvoton.com>
2025-12-09 18:31:42 +0800

    Update ADC sample code to ensure stable, continuous conversions
    Change-Id: I064cb709df4fc556fae02304d210c8e1220cc4a9

--- SampleCode/StdDriver/ADC_Convert/main.c
+++ SampleCode/StdDriver/ADC_Convert/main.c
@@ -32,10 +32,12 @@ void SYS_Init(void)
     CLK_EnableModuleClock(ADC_MODULE);
     CLK_EnableModuleClock(GPB_MODULE);
     CLK_EnableModuleClock(UART0_MODULE);
+    CLK_EnableModuleClock(TMR0_MODULE);
 
     /* Select IP clock source */
     CLK_SetModuleClock(ADC_MODULE, 0, CLK_CLKDIV4_ADC(900));  // Set ADC clock rate to 200kHz
     CLK_SetModuleClock(UART0_MODULE, CLK_CLKSEL2_UART0SEL_HXT, CLK_CLKDIV1_UART0(1));
+    CLK_SetModuleClock(TMR0_MODULE, CLK_CLKSEL1_TMR0SEL_HXT, 0);
 
     /* Set PB.12 to input mode */
     PB->MODE &= ~GPIO_MODE_MODE12_Msk;
@@ -93,6 +95,8 @@ int32_t main (void)
         // Trigger ADC conversion if it is idle
         if(!u32Busy)
         {
+            /* Delay 10 us for ADC conversion */
+            TIMER_Delay(TIMER0, 10);
             u32Busy = 1;
             ADC_START_CONV(ADC0);
         }
